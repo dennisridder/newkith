bfb97d46504c99f95f932f7525d5881306bf510f
Dries Bos <info@driesbos.com>
2020-08-04 10:19:07 +0200

    prettier rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,7 +23,7 @@ module.exports = {
     semi: [2, "never"],
     "no-console": "off",
     "vue/max-attributes-per-line": "off",
-    "prettier/prettier": ["error", { semi: false }],
+    "prettier/prettier": ["error", { semi: false, endOfLine: "auto" }],
     "vue/require-default-prop": "off",
     "vue/no-v-html": "off"
   }
