17eb0b70daabf9f6e8656c9dc79c12297838fc99
Dries Bos <driesbos@MBP-van-Dries.home>
2020-08-21 20:40:55 +0200

    refactor: vertical spacing

--- mixins/titleScrollSpeed.js
+++ mixins/titleScrollSpeed.js
@@ -1,40 +1,40 @@
-import { gsap } from "gsap"
-import { ScrollTrigger } from "gsap/ScrollTrigger"
-gsap.registerPlugin(ScrollTrigger)
+// import { gsap } from "gsap"
+// import { ScrollTrigger } from "gsap/ScrollTrigger"
+// gsap.registerPlugin(ScrollTrigger)
 
-export default {
-  mounted() {
-    this.scrollSpeedTitles()
-  },
-  methods: {
-    scrollSpeedTitles() {
-      console.log("FIRED")
-      var titles = document.querySelectorAll("#scrollSlow")
-      // Get the last el
-      // const lastIndex = titles.length - 1
-      titles.forEach((el, i) => {
-        console.log(el)
-        gsap.fromTo(
-          el,
-          {
-            y: "0vh"
-            // y: () => (i == 0 ? "0vh" : "-20vh")
-          },
-          {
-            y: "50vh",
-            ease: "none",
-            scrollTrigger: {
-              trigger: el,
-              scrub: true,
-              // First el has a different starting point
-              start: () => (i == 0 ? "center center" : "top bottom"),
-              // end: () => (i == lastIndex ? "top top" : "bottom top")
-              end: "bottom top"
-              // markers: true
-            }
-          }
-        )
-      })
-    }
-  }
-}
+// export default {
+//   mounted() {
+//     this.scrollSpeedTitles()
+//   },
+//   methods: {
+//     scrollSpeedTitles() {
+//       console.log("FIRED")
+//       var titles = document.querySelectorAll("#scrollSlow")
+//       // Get the last el
+//       // const lastIndex = titles.length - 1
+//       titles.forEach((el, i) => {
+//         console.log(el)
+//         gsap.fromTo(
+//           el,
+//           {
+//             y: "0vh"
+//             // y: () => (i == 0 ? "0vh" : "-20vh")
+//           },
+//           {
+//             y: "50vh",
+//             ease: "none",
+//             scrollTrigger: {
+//               trigger: el,
+//               scrub: true,
+//               // First el has a different starting point
+//               start: () => (i == 0 ? "center center" : "top bottom"),
+//               // end: () => (i == lastIndex ? "top top" : "bottom top")
+//               end: "bottom top"
+//               // markers: true
+//             }
+//           }
+//         )
+//       })
+//     }
+//   }
+// }
