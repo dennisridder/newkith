4b9d4d48c877c9ac2f3cdf8756c76bec5c819b00
Dries Bos <driesbos@MBP-van-Dries.home>
2020-08-23 15:16:58 +0200

    fix: data type mismatch

--- components/ImageContainerTilt.vue
+++ components/ImageContainerTilt.vue
@@ -11,7 +11,7 @@ import gsap from "gsap"
 import $ from "jquery"
 
 export default {
-  props: { image: String, title: String, id: Number },
+  props: { image: String, title: String, id: String },
   mounted() {},
   methods: {
     imageTilt: function(event) {
