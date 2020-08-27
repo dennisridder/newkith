c1d8c3816b1394afe73f79aa7cdf13f8acd15599
Dries Bos <driesbos@MBP-van-Dries.home>
2020-08-27 20:04:03 +0200

    fix: imageGrid image alignment

--- components/ImageGrid.vue
+++ components/ImageGrid.vue
@@ -122,28 +122,26 @@ export default {
     flex-direction: row
     flex-direction: column
     .imageGrid-Row
-      &:nth-child(1n)
+      &:nth-child(1), &:nth-child(11), &:nth-child(21), &:nth-child(31), &:nth-child(41)
         justify-content: flex-end
-      &:nth-child(2n)
+      &:nth-child(2), &:nth-child(12), &:nth-child(22), &:nth-child(32), &:nth-child(42)
         justify-content: flex-start
-      &:nth-child(3n)
+      &:nth-child(3), &:nth-child(13), &:nth-child(23), &:nth-child(33), &:nth-child(43)
         justify-content: flex-end
-      &:nth-child(4n)
+      &:nth-child(4), &:nth-child(14), &:nth-child(24), &:nth-child(34), &:nth-child(44)
         justify-content: center
-      &:nth-child(5n)
+      &:nth-child(5), &:nth-child(15), &:nth-child(25), &:nth-child(35), &:nth-child(45)
         justify-content: flex-end
-      &:nth-child(6n)
+      &:nth-child(6), &:nth-child(16), &:nth-child(26), &:nth-child(36), &:nth-child(46)
         justify-content: flex-start
-      &:nth-child(7n)
+      &:nth-child(7), &:nth-child(17), &:nth-child(27), &:nth-child(37), &:nth-child(47)
         justify-content: flex-end
-      &:nth-child(8n)
+      &:nth-child(8), &:nth-child(18), &:nth-child(28), &:nth-child(38), &:nth-child(48)
         justify-content: flex-start
-      &:nth-child(9n)
+      &:nth-child(9), &:nth-child(19), &:nth-child(29), &:nth-child(39), &:nth-child(49)
         justify-content: flex-end
-      &:nth-child(10n)
+      &:nth-child(10), &:nth-child(20), &:nth-child(30), &:nth-child(40), &:nth-child(50)
         justify-content: flex-start
-      &:last-child(2)
-        justify-content: center
       &:last-child
         justify-content: flex-end
         .imageGrid-Item
