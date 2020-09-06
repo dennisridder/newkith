4ebb2c7217fc001ef3a8f7d50adaac7e3d91e3d3
Dries Bos <driesbos@MBP-van-Dries.home>
2020-09-06 13:19:56 +0200

    update: carouselOnScroll mobile speed

--- mixins/carouselOnScroll.js
+++ mixins/carouselOnScroll.js
@@ -9,44 +9,86 @@ export default {
   },
   methods: {
     onScroll() {
-      const sectionWidth = window.innerWidth / 1.5
+      const mq = window.matchMedia("(min-width: 768px)")
+
       setTimeout(function() {
-        var targets = document.querySelectorAll(".carouselOnScroll")
-        targets.forEach((el, index) => {
-          var child = el.querySelectorAll(".carouselOnScroll-Wrapper")
-          if (index % 2 === 0) {
-            gsap.fromTo(
-              child,
-              {
-                x: 0
-              },
-              {
-                x: -sectionWidth,
-                ease: "none",
-                scrollTrigger: {
-                  trigger: el,
-                  scrub: true,
-                  start: "top bottom",
-                  end: "bottom top"
+        if (mq.matches) {
+          const sectionWidth = window.innerWidth
+          var targets = document.querySelectorAll(".carouselOnScroll")
+          targets.forEach((el, index) => {
+            var child = el.querySelectorAll(".carouselOnScroll-Wrapper")
+            if (index % 2 === 0) {
+              gsap.fromTo(
+                child,
+                {
+                  x: 0
+                },
+                {
+                  x: -sectionWidth,
+                  ease: "none",
+                  scrollTrigger: {
+                    trigger: el,
+                    scrub: true,
+                    start: "top bottom",
+                    end: "bottom top"
+                  }
                 }
-              }
-            )
-          } else {
-            gsap.fromTo(
-              child,
-              { x: -sectionWidth },
-              {
-                x: 0,
-                scrollTrigger: {
-                  trigger: el,
-                  scrub: true,
-                  start: "top bottom",
-                  end: "bottom top"
+              )
+            } else {
+              gsap.fromTo(
+                child,
+                { x: -sectionWidth },
+                {
+                  x: 0,
+                  scrollTrigger: {
+                    trigger: el,
+                    scrub: true,
+                    start: "top bottom",
+                    end: "bottom top"
+                  }
                 }
-              }
-            )
-          }
-        })
+              )
+            }
+          })
+        } else {
+          const sectionWidth = window.innerWidth * 2
+          var elements = document.querySelectorAll(".carouselOnScroll")
+          elements.forEach((el, index) => {
+            var child = el.querySelectorAll(".carouselOnScroll-Wrapper")
+            if (index % 2 === 0) {
+              gsap.fromTo(
+                child,
+                {
+                  x: 0
+                },
+                {
+                  x: -sectionWidth,
+                  ease: "none",
+                  scrollTrigger: {
+                    trigger: el,
+                    scrub: true,
+                    start: "top bottom",
+                    end: "bottom top"
+                  }
+                }
+              )
+            } else {
+              gsap.fromTo(
+                child,
+                { x: -sectionWidth },
+                {
+                  x: 0,
+                  scrollTrigger: {
+                    trigger: el,
+                    scrub: true,
+                    start: "top bottom",
+                    end: "bottom top"
+                  }
+                }
+              )
+            }
+          })
+        }
       }, 250)
     }
   }
