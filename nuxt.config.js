047863ca5a93bc93aabd70975e83aec2ced7c5dc
DriesBos <info@driesbos.com>
2021-03-17 12:58:02 +0000

    added script

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,6 +11,34 @@ module.exports = {
    */
   head: {
     title: "#NEWKITH", // Change in package.json files
+    script: [
+      {
+        innerHTML: `
+    ;(function(l, e, a, d, i, n, f, o) {
+      if (!l[i]) {
+        l.GlobalLeadinfoNamespace = l.GlobalLeadinfoNamespace || []
+        l.GlobalLeadinfoNamespace.push(i)
+        l[i] = function() {
+          ;(l[i].q = l[i].q || []).push(arguments)
+        }
+        l[i].t = l[i].t || n
+        l[i].q = l[i].q || []
+        o = e.createElement(a)
+        f = e.getElementsByTagName(a)[0]
+        o.async = 1
+        o.src = d
+        f.parentNode.insertBefore(o, f)
+      }
+    })(
+      window,
+      document,
+      "script",
+      "https://cdn.leadinfo.net/ping.js",
+      "leadinfo",
+      "LI-6047499C276BE"
+    )`
+      }
+    ],
     meta: [
       { charset: "utf-8" },
       {
